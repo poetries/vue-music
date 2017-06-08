3688530f9bd5f6fde2c38d2908922f525feeaac6
poetries <jingguanliuye@gmail.com>
2017-06-08 14:40:38 +0800

    import common/stylus/index.styl

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+//Vue.config.productionTip = false
+
+import 'common/stylus/index.styl'
 
 /* eslint-disable no-new */
 new Vue({
