69601d36e0d1df63e822b16d4c0edb1d5d60ab6b
poetries <jingguanliuye@gmail.com>
2017-06-08 19:04:27 +0800

    完成tab的切换

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,10 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
+      path: '/',
+      redirect: '/recommend'
+    },
+    {
       path: '/recommend',
       component: Recommend
     },
