032c6a970a295772d71854cffc1d49c05bfb8ab3
poetries <jingguanliuye@gmail.com>
2017-06-09 21:09:55 +0800

    完善singer组件

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,11 +1,83 @@
 <template>
-	<div>歌手页面</div>
+	<div class="singer"></div>
 </template>
 
 <script type="text/ecmascript-6">
+   import {getSingerList} from 'api/singer'
+   import {ERR_OK} from 'api/config'
+   import Singer from 'common/js/singer'
+	
+   const HOT_NAME = '热门'
+   const HOT_SINGRE_LEN = 10
 
+   export default {
+   	 data() {
+   	 	return {
+   	 		singers: []
+   	 	}
+   	 },
+   	 created() {
+   	 	this._getSingerList()
+   	 },
+   	 methods: {
+		_getSingerList() {
+			getSingerList().then((res) => {
+			   if (res.code === ERR_OK) {
+			   		this.singers = res.data.list
+			   		console.log(this._normalizeSinger(this.singers))
+			   }
+			})
+		},
+		_normalizeSinger(list) {
+			let map = {
+			  hot: {
+			     title: HOT_NAME,
+			     items: []
+			  }
+			}
+			list.forEach((item, index) => {
+			  if (index < HOT_SINGRE_LEN) {
+			    map.hot.items.push(new Singer({
+			      name: item.Fsinger_name,
+                  id: item.Fsinger_mid
+			    }))
+			  }
+			  const key = item.Findex
+			  if (!map[key]) {
+			    map[key] = {
+			      title: key,
+			      items: []
+			    }
+			  }
+			  map[key].items.push(new Singer({
+				  name: item.Fsinger_name,
+                  id: item.Fsinger_mid
+			  }))
+			})
+			// 为了得到有序列表，我们需要处理 map
+	        let ret = []
+	        let hot = []
+	        for (let key in map) {
+	          let val = map[key]
+	          if (val.title.match(/[a-zA-Z]/)) {
+	            ret.push(val)
+	          } else if (val.title === HOT_NAME) {
+	            hot.push(val)
+	          }
+	        }
+	        ret.sort((a, b) => {
+	          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+	        })
+	        return hot.concat(ret)
+		} 
+   	 }
+   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-
+	.singer
+	    position: fixed
+	    top: 88px
+	    bottom: 0
+	    width: 100%
 </style>
