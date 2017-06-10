94a390a73f350b5217e3a4b952c48c604749bd66
poetries <jingguanliuye@gmail.com>
2017-06-10 22:38:18 +0800

    完善listview右侧字母列表滚动触摸事件

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -1,7 +1,7 @@
 <template>
-<scroll class="listview" :data="data">
+<scroll class="listview" :data="data" ref="listview">
   <ul>
-    <li v-for="group in data" class="list-group">
+    <li v-for="group in data" class="list-group" ref="listGroup">
       <h2 class="list-group-title">{{group.title}}</h2>
       <ul>
         <li v-for="item in group.items" class="list-group-item">
@@ -11,9 +11,9 @@
       </ul>
     </li>
   </ul>
-  <div class="list-shortcut">
+  <div class="list-shortcut" @touchstart="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove">
     <ul>
-      <li v-for="item in shortcutList" class="item">{{item}}</li>
+      <li v-for="(item,index) in shortcutList" class="item" :data-index="index">{{item}}</li>
     </ul>
   </div>
 </scroll>
@@ -21,8 +21,14 @@
 
 <script type="text/ecmascript-6">
   import Scroll from 'base/scroll/scroll'
+  import {getData} from 'common/js/dom'
+
+  const ANCHOR_HEIGHT = 18
 
   export default {
+    created() {
+      this.touch = {}
+    },
     props: {
       data: {
         type: Array,
@@ -34,6 +40,26 @@
         return this.data.map((group) => {
           return group.title.substr(0,1)
         })
+      }
+    }, 
+    methods: {
+      onShortcutTouchStart(e) {
+        let anchorIndex = getData(e.target, 'index')
+        let firstTouch = e.touches[0]
+        this.touch.y1 = firstTouch.pageY
+        this.touch.anchorIndex = anchorIndex
+        this._scrollTo(anchorIndex)
+      },
+      onShortcutTouchMove(e) {
+        let firstTouch = e.touches[0]
+        this.touch.y2 = firstTouch.pageY
+        let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0
+        let anchorIndex = parseInt(this.touch.anchorIndex) + delta
+        console.log(anchorIndex)
+        this._scrollTo(anchorIndex)
+      },
+      _scrollTo(index) {
+         this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)
       }
     },
     components: {
