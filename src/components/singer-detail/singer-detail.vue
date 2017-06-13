d0b321ea4e16e074c6cd83fec3ac3a494599e1ce
poetries <jingguanliuye@gmail.com>
2017-06-13 20:41:50 +0800

    完成singer-detail路由的配置以及转场动画

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,5 +1,7 @@
 <template>
+<transition name="slide">
   <div class="singer-detail"></div>
+</transition>
 </template>
 
 <script type="text/ecmascript-6">
@@ -17,4 +19,9 @@
     bottom: 0
     right: 0
     background: $color-background
+
+  .slide-enter-active, .slide-leave-active
+    transition: all 0.3s
+  .slide-enter, .slide-leave-to
+    transform: translate3d(100%, 0, 0)    
 </style>
