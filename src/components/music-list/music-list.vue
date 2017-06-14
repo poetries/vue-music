709e20a8f4ec24bf851f581cc1ad45456e48fc67
poetries <jingguanliuye@gmail.com>
2017-06-14 11:09:00 +0800

    完善music-list组件

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -68,8 +68,19 @@
     watch: {
       scrollY(newY) {
         let translateY = Math.max(this.minTranslateY, newY)
+        let zIndex= 0
         this.$refs.layer.style['transform'] = `translate3d(0, ${translateY}px, 0)` 
         this.$refs.layer.style['webkitTransform'] = `translate3d(0, ${translateY}px, 0)` 
+
+        if (newY < this.minTranslateY) {
+          zIndex = 10
+          this.$refs.bgImage.style.paddingTop = 0
+          this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+        } else {
+          this.$refs.bgImage.style.paddingTop = '70%'
+          this.$refs.bgImage.style.height = 0
+        }
+        this.$refs.bgImage.style.zIndex  =zIndex
       }
     },
     components: {
