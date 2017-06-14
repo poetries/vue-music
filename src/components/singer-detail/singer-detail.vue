90a3d21be0ab2f81756549d46a4fd4ca6f5e5b1f
poetries <jingguanliuye@gmail.com>
2017-06-14 09:20:00 +0800

    music-list组件开发

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,6 @@
 <template>
 <transition name="slide">
-  <div class="singer-detail"></div>
+  <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
 </transition>
 </template>
 
@@ -9,6 +9,7 @@
   import {getSingerDetail} from 'api/singer'
   import {ERR_OK} from 'api/config'
   import {createSong} from 'common/js/song'
+  import MusicList from 'components/music-list/music-list'
 
   export default {
     data() {
@@ -17,6 +18,12 @@
       }
     },
     computed: {
+      title() {
+        return this.singer.name
+      },
+      bgImage() {
+        return this.singer.avatar 
+      },
       ...mapGetters([
         'singer'
       ])
@@ -46,21 +53,15 @@
         }) 
         return ret
       }
+    },
+    components: {
+      MusicList
     }
   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
-
-  .singer-detail
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    bottom: 0
-    right: 0
-    background: $color-background
 
   .slide-enter-active, .slide-leave-active
     transition: all 0.3s
