61490d6e7a79a1006303e588d02b6149fee6c6c2
poetries <jingguanliuye@gmail.com>
2017-06-14 17:11:42 +0800

    finish vuex数据设计

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,1 +1,17 @@
 export const singer = state => state.singer
+
+export const playing = state => state.playing
+
+export const fullScreen = state => state.fullScreen
+
+export const playlist = state => state.playlist
+
+export const sequenceList = state => state.sequenceList
+
+export const mode = state => state.mode
+
+export const currentIndex = state => state.currentIndex
+
+export const currentSong = (state) => {
+	return state.playing[state.currentIndex] || {}
+}
--- src/store/state.js
+++ src/store/state.js
@@ -1,4 +1,12 @@
+import {playMode} from 'common/js/config'
+
 const state = {
-	singer: {}
+	singer: {},
+	playing: false,
+	fullScreen: false,
+	playlist: [],
+	sequenceList: [],
+	mode: playMode.sequence,
+	currentIndex: -1
 }
 export default state
